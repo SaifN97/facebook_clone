4ba4d09f832f38c00aa825c93cd77443d4e4319a
SaifN97 <saifnarpali@gmail.com>
2021-02-07 18:47:28 +0530

    User can now post text on newsfeed

--- resources/js/store/modules/posts.js
+++ resources/js/store/modules/posts.js
@@ -1,6 +1,7 @@
 const state = {
     newsPosts: null,
-    newsPostsStatus: null
+    newsPostsStatus: null,
+    postMessage: ''
 };
 
 const getters = {
@@ -12,6 +13,10 @@ const getters = {
         return {
             newsPostsStatus: state.newsPostsStatus
          }
+    },
+
+    postMessage: state => {
+        return state.postMessage;
     }
 };
 
@@ -28,6 +33,19 @@ const actions = {
                 commit('setPostsStatus', 'error');
                 console.log('unable to fetch posts');
             });
+    },
+
+    postMessage({ commit, state }) {
+        commit('setPostsStatus', 'loading');
+        axios.defaults.headers.common['Authorization'] = 'Bearer ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjczNWY3NWY2MDNkNmM0ODk1MjEwOTQ5Y2RlNGUyYzU2NjQwOTQ0MTkyNWMyOThlOWM4NWQ1YzlmMzU5ZTQyYjhmZWQxMjk2NGNkZWMxNTgwIn0.eyJhdWQiOiIyIiwianRpIjoiNzM1Zjc1ZjYwM2Q2YzQ4OTUyMTA5NDljZGU0ZTJjNTY2NDA5NDQxOTI1YzI5OGU5Yzg1ZDVjOWYzNTllNDJiOGZlZDEyOTY0Y2RlYzE1ODAiLCJpYXQiOjE2MTE1NzAyMDUsIm5iZiI6MTYxMTU3MDIwNSwiZXhwIjoxNjQzMTA2MjA1LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.nqXisW4QPcHzznp1UwUWqPPNhabKT5TL8l05JRtSiDx59BS4AeS4WsjfqzWxJdKNDYR8xDsimZi82wtTuB2HfcxORdABt0nOtF2-n4e1uI_HXLQve1R7l8iZ8LuekeBYDp_-YADJ2TqHHOi4CkD9c9vcSP0Ka10_-wMHDS8ugB2_Dfi1D2sZDoUDbYrakpZ8Pa02keTdbMlgrEDnD_BIcDJxgo6mP5-olwSibp0zSFIkNODWhQ5ix1OOm3n3nYAJFUH2EYkYG_QdrF4UU4T5hjRTWDWj1Jxjwmi-KEgQLyQqPV7iuT4sVYaAJt5Nuzn4LNDoLys2bWiLJoLC1n2sjl5oj2HunJR-x4Ci_sTb5iEHx5det0GPrc50jD3o65k4LSbVOPamdT5z54RXJ3Iri7vgZU_3b6MX3ZKnngBTqThmRsOHATBpdfkxMh9NoRgVO47h35Fml75IIlMpi4kz4WK-C-6KRFbufAzI-zSYylOm9NmLnaZSFzl_Acdc8ejcmZQT40-wPFkV7k4NoBnsB9QoFaHwnMzG0sK3OhWJIP0sWyMdC3j7vexk0TzPnZxJ2cIoJs-6OLHjp2YBlFkxpMd4_1zMpcDvZMJ1dgqoOUyON5C5jnGuyXTxyHJt6EpkP7h6y5nx8cU6IzkGbrdkOvTBASVbNvciXaSc8FX4PUQ'
+        axios.post('/api/posts', {body: state.postMessage})
+            .then(res => {
+                commit('pushPost', res.data);
+                commit('updateMessage', '');
+            })
+            .catch(error => {
+
+            });
     }
 };
 
@@ -35,9 +53,18 @@ const mutations = {
     setPosts(state, posts) {
         state.newsPosts = posts;
     },
+
     setPostsStatus(state, status) {
         state.newsPostsStatus = status;
     },
+
+    updateMessage(state, message) {
+        state.postMessage = message;
+    },
+
+    pushPost(state, post) {
+        state.newsPosts.data.unshift(post);
+    }
 };
 
 export default {
